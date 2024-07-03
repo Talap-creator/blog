0237686ba648b2dfd6a82160888e558bde590d5d
terfo1 <alisher.temirhan@gmail.com>
2024-07-03 23:56:51 +0500

    staticfiles upd

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -109,8 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CKEDITOR_5_UPLOAD_PATH = 'uploads/'
