17b00f88646f4f3a0833e1a25002d4bb7a6655c3
Bakkenti <147378518+Bakkenti@users.noreply.github.com>
2024-07-15 18:03:25 +0500

    Update settings.py

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = []
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CKEDITOR_5_UPLOAD_PATH = 'uploads/'
 CKEDITOR_5_CONFIGS = {
