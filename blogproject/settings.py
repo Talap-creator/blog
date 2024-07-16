65e18c2266d967a09548f7733616f1b591092660
Bakkenti <147378518+Bakkenti@users.noreply.github.com>
2024-07-17 03:46:22 +0500

    Update settings.py

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://ofy-back-3inrc.ondigitalocean.app/'
+    'https://ofy-back-3inrc.ondigitalocean.app'
 ]
 
 MIDDLEWARE = [
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'workspace/staticfiles/')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 CKEDITOR_5_UPLOAD_PATH = 'uploads/'
 CKEDITOR_5_CONFIGS = {
