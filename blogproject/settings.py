c557d8e32e32c59284ec1fb44f39a9b392a47797
Bakkenti <147378518+Bakkenti@users.noreply.github.com>
2024-07-17 03:29:29 +0500

    Update settings.py

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -8,7 +8,7 @@ from django.core.management.utils import get_random_secret_key
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = [
-    'ofy-back-3inrc.ondigitalocean.app/'
+    'ofy-back-3inrc.ondigitalocean.app'
 ]
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
