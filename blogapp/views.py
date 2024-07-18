f0bf674c2f7efc48f020f0fa1ca2323b77428130
Bakkenti <147378518+Bakkenti@users.noreply.github.com>
2024-07-18 17:08:13 +0500

    Update views.py

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 import os
 from .models import BlogPost
 
@@ -7,14 +8,14 @@ def blogs(request):
     posts = BlogPost.objects.all()
     data = {
         'blogs': [{
-            'id':post.id,
+            'id': post.id,
             'category': post.category,
             'title': post.title,
             'content': post.content,
             'short_description': post.short_description,
             'created_at': post.created_at.strftime('%d %B, %H:%M'),
             'updated_at': post.updated_at.strftime('%d %B, %H:%M'),
-            'image':post.image.url if post.image else None,
+            'image': post.image.url if post.image else None,
         } for post in posts]
     }
     return JsonResponse(data)
@@ -24,12 +25,18 @@ def ck_editor_5_upload_file(request):
     if request.method == 'POST' and request.FILES.get('upload'):
         uploaded_file = request.FILES['upload']
         file_path = os.path.join(settings.MEDIA_ROOT, uploaded_file.name)
-        file_url = settings.MEDIA_URL + uploaded_file.name  # Ensure correct URL format
+        file_url = settings.MEDIA_URL + uploaded_file.name
+
         try:
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
             with open(file_path, 'wb+') as destination:
                 for chunk in uploaded_file.chunks():
                     destination.write(chunk)
+
             return JsonResponse({'uploaded': True, 'url': file_url})
+        
         except Exception as e:
             return JsonResponse({'uploaded': False, 'error': str(e)})
+    
     return JsonResponse({'uploaded': False, 'error': 'Invalid request'})
