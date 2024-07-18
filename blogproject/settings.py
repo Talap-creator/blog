be36396433a990774989df325f36b6c3d376c981
Bakkenti <147378518+Bakkenti@users.noreply.github.com>
2024-07-18 16:30:22 +0500

    Update settings.py

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -134,7 +134,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-CKEDITOR_5_UPLOAD_PATH = 'uploads/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+CKEDITOR_5_UPLOAD_PATH = 'media/blog_images'
 CKEDITOR_5_CONFIGS = {
     'default': {
         "toolbar": [
@@ -159,6 +161,3 @@ CKEDITOR_5_CONFIGS = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
