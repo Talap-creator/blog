8f18a93a142d13cf3649ab865193b902415f1a53
Bakkenti <147378518+Bakkenti@users.noreply.github.com>
2024-07-24 13:17:59 +0500

    Update views.py

--- blogapp/views.py
+++ blogapp/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 import os
 from .models import BlogPost
+from django.core.files.storage import default_storage
 
 def blogs(request):
     posts = BlogPost.objects.all()
@@ -23,19 +24,13 @@ def blogs(request):
 @csrf_exempt
 def ck_editor_5_upload_file(request):
     if request.method == 'POST' and request.FILES.get('upload'):
-        uploaded_file = request.FILES.get('upload')
-        response = upload(request)
-        
-        # Check if it's a single file or multiple files
-        if isinstance(response, list):
-            # Multiple files uploaded
-            urls = [file_data['url'] for file_data in response]
-        else:
-            # Single file uploaded
-            urls = [response.url]
+        uploaded_file = request.FILES['upload']
+        save_path = os.path.join(settings.MEDIA_ROOT, 'uploads', uploaded_file.name)
+        path = default_storage.save(save_path, uploaded_file)
+        url = default_storage.url(path)
 
         return JsonResponse({
-            'urls': urls,
+            'url': url,
             'uploaded': True,
         })
     return JsonResponse({'error': 'Invalid request method or no file uploaded'}, status=400)
