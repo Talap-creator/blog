5d5ceadfb60f8ba3bbb56a57368230fe69fa0367
Bakkenti <147378518+Bakkenti@users.noreply.github.com>
2024-07-24 14:14:47 +0500

    Update urls.py

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -5,3 +5,6 @@ urlpatterns = [
     path('', views.blogs, name='blogs'),
     path('ckeditor/upload/', views.ck_editor_5_upload_file, name='ck_editor_5_upload_file'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
