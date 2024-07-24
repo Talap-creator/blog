fa5cf38a77d91f516f2b9329a088e9e783453e3d
Bakkenti <147378518+Bakkenti@users.noreply.github.com>
2024-07-24 13:25:37 +0500

    Update urls.py

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.blogs, name='blogs'),
-    path('ckeditor/upload/', views.ck_editor_5_upload_file, name='ck_editor_5_upload_file')
+    path('ckeditor/upload/', views.ck_editor_5_upload_file, name='ck_editor_5_upload_file'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
