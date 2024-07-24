ee7b180dfac3db85aa8fb8e56321ca40d30d4ca1
Bakkenti <147378518+Bakkenti@users.noreply.github.com>
2024-07-24 16:07:46 +0500

    Update urls.py

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogapp.urls')),
+    path('blog/', include('blogapp.urls')), 
 ]
-urlpatterns  +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
