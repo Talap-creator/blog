485ee91a1d682ad552a5aabf10ea3c425b4fed1a
Bakkenti <147378518+Bakkenti@users.noreply.github.com>
2024-07-24 13:19:15 +0500

    Update admin.py

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -5,13 +5,12 @@ from django_ckeditor_5.fields import CKEditor5Widget
 
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'short_description', 'created_at', 'updated_at')
-    search_fields = ('title', 'category','content', 'short_description')
-    fields = ('title', 'category','content', 'short_description','image')
+    search_fields = ('title', 'category', 'content', 'short_description')
+    fields = ('title', 'category', 'content', 'short_description', 'image')
 
     def formfield_for_dbfield(self, db_field, **kwargs):
         if db_field.name == 'content':
-            return db_field.formfield(widget=CKEditor5Widget())
+            return db_field.formfield(widget=CKEditor5Widget(config_name='default'))
         return super().formfield_for_dbfield(db_field, **kwargs)
-
 
 admin.site.register(BlogPost, BlogPostAdmin)
