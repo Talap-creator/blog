2b4d446925ef3d40b6bc8f35848af8b341a3a4f0
Bakkenti <147378518+Bakkenti@users.noreply.github.com>
2024-07-24 14:11:31 +0500

    Update urls.py

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -3,12 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blogapp.urls')),
-    path('ckeditor/', include('django_ckeditor_5.urls')),  # Include CKEditor URLs
+    path('', include('blogapp.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns  +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
